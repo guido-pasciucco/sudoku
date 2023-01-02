143bf96a44d09d19d1373940f5a4fc10c5397dfa
unknown <guidopasciucco@gmail.com>
2023-01-02 09:27:54 -0300

    #2 static sudoku finished

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var numSelected = null;
-var titleSelecter = null;
-const errors = 0;
+var tileSelected = null;
 
-const board = [
+var errors = 0;
+
+var board = [
     "--74916-5",
     "2---6-3-9",
     "-----7-1-",
@@ -12,9 +13,9 @@ const board = [
     "9-4-7---2",
     "67-83----",
     "81--45---"
-];
+]
 
-const solution = [
+var solution = [
     "387491625",
     "241568379",
     "569327418",
@@ -26,49 +27,65 @@ const solution = [
     "812945763"
 ]
 
-window.onload = () => setGame();
+window.onload = function() {
+    setGame();
+}
 
-const setGame = () => {
-    /* create digits 1 - 9 */
+function setGame() {
+    // Digits 1-9
     for (let i = 1; i <= 9; i++) {
         let number = document.createElement("div");
-        number.id = i;
+        number.id = i
         number.innerText = i;
         number.addEventListener("click", selectNumber);
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
-    /* create 9x9 board */
+
+    // Board 9x9
     for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++){
+        for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
-            tile.id = `${r.toString()}-${c.toString()}`;
-            
-            tile.addEventListener("click", selectTile)
-            
+            tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").append(tile);
         }
     }
 }
 
-/* ver porque no funciona, no se pone el fondito gris */
-const selectNumber = () => {
-    if(numSelected != null){
-        numSelected.classList.remove("number-selected")
+function selectNumber(){
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
     }
     numSelected = this;
     numSelected.classList.add("number-selected");
-};
-
-const selectTile = () => {
-    if(numSelected){
-        this.innerText = numSelected.id;
-    }
 }
 
-
-// dejé el video en 19:50
-
-
-
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
+        let coords = this.id.split("-");
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
+    }
+}
